ee2ec88516df267590d4e90581bacc423188f4dd
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-01-19 00:23:07 +0800

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Yunpeng Niu on 19/01/18.
+//  Copyright © 2018年 Yunpeng Niu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
