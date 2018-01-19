cb178edd70877a8ee42a3db151b5df7f3c2b2524
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-01-19 09:07:34 +0800

    Two cards can be flipped now

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,7 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var flipCountLabel: UILabel!
+
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flip Count: \(flipCount)"
+        }
+    }
+
     @IBAction func touchCard(_ sender: UIButton) {
-        sender.text
+        flipCard(withEmoji: "👻", on: sender)
+        flipCount += 1
+    }
+
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🎃", on: sender)
+        flipCount += 1
+    }
+
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
     }
 }
